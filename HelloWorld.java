bf4b3c58dee2f688b7d4786e9ee33f7adb80903e
akp007 <46421398+akp007@users.noreply.github.com>
2021-03-22 16:41:56 +0530

    added add function

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
    public static void main(String [] args){
    
       System.out.println("Hello World, ZSJL has been released on HBO MAX");
-  }
+      
+   }
+   
+   public static void add(int num1,int num2){
+     System.out.println(num1+num2);  
+   }
 }
